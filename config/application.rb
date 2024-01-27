f31bcdb4e83a3f9b12cbe41f3c331c2c108d30da
robinski <robinskilling@gmail.com>
2024-01-27 10:49:40 +0000

    update generators

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,10 @@ module Wildcat
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.view_specs false
+      g.helper_specs false
+      g.helper false
+    end
   end
 end
