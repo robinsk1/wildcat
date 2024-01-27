688235d6bb0b5500ab294a1846edc0490037af69
robinski <robinskilling@gmail.com>
2024-01-27 13:01:05 +0000

    create within and contains methods in model

--- app/models/wildcat_priority_area.rb
+++ app/models/wildcat_priority_area.rb
@@ -1,2 +1,15 @@
 class WildcatPriorityArea < ApplicationRecord
+  def self.contains(lat, lon)
+    point = RGeo::Geos.factory(srid: 4326).point(lat, lon)
+    areas = WildcatPriorityArea.arel_table
+    WildcatPriorityArea.where(areas[:wkb_geometry].st_contains(point))
+  end
+
+  def self.within(xmin, ymin, xmax, ymax)
+    se = RGeo::Geos.factory(srid: 4326).point(xmin, ymin)
+    nw = RGeo::Geos.factory(srid: 4326).point(xmax, ymax)
+    box = RGeo::Cartesian::BoundingBox.create_from_points(se, nw)
+    areas = WildcatPriorityArea.arel_table
+    WildcatPriorityArea.where(areas[:wkb_geometry].st_within(box))
+  end
 end
--- test/models/wildcat_priority_area_test.rb
+++ test/models/wildcat_priority_area_test.rb
@@ -1,7 +1,41 @@
 require "test_helper"
 
 class WildcatPriorityAreaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'extents with polygon' do 
+    bermuda = [26.468840334661323, 
+               -74.95585551599015,
+               37.6126981403826,
+               -54.76298442224015]
+
+    result = WildcatPriorityArea.within(*bermuda)
+
+    assert 1, result.count
+  end
+
+  test 'extents no polygon' do 
+    iceland = [62.13286920942739,
+               -29.04885991271948,
+               67.69961702955956,
+               -8.855988818969536]
+
+    result = WildcatPriorityArea.within(*iceland)
+
+    assert 0, result.count
+  end
+
+  test 'gets zones inside' do
+    bermuda = [25.346688401045476, -71.73249306285165]
+
+    result = WildcatPriorityArea.contains(*bermuda)
+
+    assert 1, result.count
+  end
+
+  test 'gets no zones outside' do
+    iceland = [63.71908132538131, -20.02195210736899]
+
+    result = WildcatPriorityArea.contains(*iceland)
+
+    assert 1, result.count
+  end
 end
