224219106038008b9085c78a227713fd94499e3e
robinski <robinskilling@gmail.com>
2024-01-28 12:11:11 +0000

    render fiddle

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,7 @@
+#!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 bundle install
-bundle exec rails db:create
-bundle exec rails db:migrate
-
-bundle exec rake data:wildcat_priority_area_import
+./bin/rails assets:precompile
+./bin/rails assets:clean
